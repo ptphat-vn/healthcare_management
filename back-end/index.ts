6108aa8621665baba3fdc99ddefa0b380a0748ed
ptphat-vn <tanphatphan091@gmail.com>
2025-10-08 08:43:53 +0700

    create folder BE

---
+++ back-end/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+app.listen(PORT, () => {
+  console.log(`Project này đang chạy trên post ${PORT}`)
+})
