868b9762bd94460e46e6566680fdf53bb068d7a5
minhkiet1001 <minhkiethp123@gmail.com>
2025-10-09 17:19:09 +0700

    Thêm endpoint tạo người dùng và phản hồi lỗi chi tiết

--- back-end/src/routes/user.routes.ts
+++ back-end/src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { validateLogin, validateRegister, validateForgotPassword, validateResetPassword, validateChangePassword } from '~/middlewares/users.middlewares'
-import { loginController, logoutController, registerController, forgotPasswordController, resetPasswordController, changePasswordController } from '~/controllers/user.controllers'
+import { loginController, logoutController, registerController, forgotPasswordController, resetPasswordController, changePasswordController, createUserController } from '~/controllers/user.controllers'
 import { authMiddleware } from '~/middlewares/auth.middleware'
 
 const userRouter = Router()
 
 userRouter.post('/register', validateRegister, registerController)
+userRouter.post('/create-user', validateRegister, createUserController)
 userRouter.post('/login', validateLogin, loginController)
 userRouter.post('/logout', logoutController)
 userRouter.post('/forgot-password', validateForgotPassword, forgotPasswordController)
