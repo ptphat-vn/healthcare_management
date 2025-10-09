d5604ebcee1004288d1e4ca50007c850e5513dfc
NBao261 <ncb2601@gmail.com>
2025-10-10 00:21:20 +0700

    refactor: đổi route register thành auth-regis, login thành auth-login

--- back-end/src/routes/user.routes.ts
+++ back-end/src/routes/user.routes.ts
@@ -5,9 +5,9 @@ import { authMiddleware } from '~/middlewares/auth.middleware'
 
 const userRouter = Router()
 
-userRouter.post('/register', validateRegister, registerController)
+userRouter.post('/auth-regis', validateRegister, registerController)
 userRouter.post('/create-user', validateRegister, createUserController)
-userRouter.post('/login', validateLogin, loginController)
+userRouter.post('/auth-login', validateLogin, loginController)
 userRouter.post('/logout', logoutController)
 userRouter.post('/forgot-password', validateForgotPassword, forgotPasswordController)
 userRouter.post('/reset-password', validateResetPassword, resetPasswordController)
