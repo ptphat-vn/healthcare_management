36445142dc86bca63b10d52b6bed843446ddb986
ptphat-vn <tanphatphan091@gmail.com>
2025-11-26 09:35:15 +0700

    fix: fix conflict login

--- front-end/src/components/features/chat/ChatWindow/ChatWindow.tsx
+++ front-end/src/components/features/chat/ChatWindow/ChatWindow.tsx
@@ -15,7 +15,7 @@ import { useConversations } from "../../../../hooks/useConversations";
 import LoadingSpinner from "@/components/ui/loading/LoadingSpinner";
 import EmptyState from "@/components/ui/empty/EmptyState";
 import EmojiPickerButton from "@/components/ui/emoji/EmojiPickerButton";
-import VideoCallButton from "../videoCall/VideoCallButton/VideoCallButton";
+import VideoCallButton from "@/components/features/videoCall/VideoCallButton/VideoCallButton";
 interface ChatWindowProps {
   otherUserId: string;
   otherUserName: string;
